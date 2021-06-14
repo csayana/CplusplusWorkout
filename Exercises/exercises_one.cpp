95d02405758272828c85ba9f636c5876b9dcb968
Can Seran AYANA <csayanadesign@gmail.com>
2021-06-14 13:19:17 +0300

    Create exercises one
    C++ Comment example

---
+++ Exercises/exercises_one.cpp
@@ -0,0 +1,11 @@
+int x = 2; // A single-line comment can appear at the end of a line.
+
+// Single-line comments can also appear by themselves on a line.
+
+int x = 2;
+
+/* This is a multi-line comment.
+   It will continue for as many lines as necessary
+   until it's closed with the closing marker. */
+
+int y = 3;
